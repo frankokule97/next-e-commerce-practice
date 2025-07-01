8d965d6efab470b67a74d94f58b55a22bf0dbfc8
frankokule97 <franko.kule97@gmail.com>
2025-07-01 17:12:53 +0200

    working on products page and adding links and images and other implementations

--- src/app/(store)/products/page.tsx
+++ src/app/(store)/products/page.tsx
@@ -1,5 +1,37 @@
+import { categories, productsList } from "@/app/data/products"
+import Link from "next/link"
+import Image from "next/image"
+
+
 export default function ProductsPage() {
+    const getCategoryImage = (categorySlug: string): string => {
+        const categoryProduct = productsList.find(product => product.category === categorySlug)
+        return categoryProduct?.image || "/images/placeholder.png"
+    }
     return (
-        <div className="pt-24">Products</div>
+        <div className="max-w-7xl mx-auto px-4 pt-24 mb-16">
+            {/*Categories grid*/}
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+                {categories.map((category) => (
+                    <Link
+                        href="/products"
+                        key={category.slug}
+                        className="group bg-white rounded-xl overflow-hidden shadow-md
+                        hover:shadow-lg hover:translate-y-[-5px] border-b-4 relative transition-all"
+                        style={{borderColor: category.color}}>
+                        <div className="h-48 relative overflow-hidden bg-gray-100">
+                            <Image
+                                src={getCategoryImage(category.slug)}
+                                alt={category.name}
+                                fill
+                                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 25vw"
+                                className="object-contain p-4 group-hover:scale-105 transition-transform duration-300"
+                            />
+                        </div>
+                    </Link>
+                ))}
+            </div>
+        </div>
+
     )
 }
